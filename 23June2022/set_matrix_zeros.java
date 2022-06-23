a48bb30f2eed43525c82deda958a9e106c95b1e3
Gourav Jamdhade <74340550+gjam-oist@users.noreply.github.com>
2022-06-23 17:36:52 +0530

    Create set_matrix_zeros.java

---
+++ 23June2022/set_matrix_zeros.java
@@ -0,0 +1,38 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        //Brute Force
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    //Setting row to -1
+                    for(int k = 0; k < matrix[0].length; k++ ){
+                        if(matrix[i][k] != 0){
+                            matrix[i][k] = -1;
+                        }
+                    }
+                    //Setting col to -1
+                    for(int k = 0; k < matrix.length; k++ ){
+                        if(matrix[k][j] != 0){
+                            matrix[k][j] = -1;
+                        }
+                    }
+                    
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
