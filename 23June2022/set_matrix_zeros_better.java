9fac1a44a7dafb1cb020ecc5bc73a6d842ace291
Gourav Jamdhade <74340550+gjam-oist@users.noreply.github.com>
2022-06-23 17:50:34 +0530

    Create set_matrix_zeros_better.java

---
+++ 23June2022/set_matrix_zeros_better.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        //The better one
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int row[] = new int[m];
+        int col[] = new int[n];
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = -1;
+                    col[j] = -1;
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row[i] == -1 || col[j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+}
