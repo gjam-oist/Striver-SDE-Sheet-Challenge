e15c98136862ad1e4fdff0ed57847b2d405cfb6c
Gourav Jamdhade <74340550+gjam-oist@users.noreply.github.com>
2022-07-03 01:46:27 +0530

    Update rotate_matrix.java

--- 2july2022/rotate_matrix.java
+++ 2july2022/rotate_matrix.java
@@ -5,62 +5,60 @@ import java.util.ArrayList;
 public class Solution {
     public static void rotateMatrix(ArrayList<ArrayList<Integer>> mat, int n, int m) {
         int count = 0;
-    int rows = n;
-    int cols = m;
-    int total = rows * cols;
-    int curr = 0;
+        int rows = n;
+        int cols = m;
+        int total = n * m;
+        int currElement = 0;
         
         int startingRow = 0;
         int startingCol = 0;
-
         
-        while(count<total){
+        while(count < total){
+            if(startingRow + 1 == rows || startingCol + 1 == cols) break;
             
-            if(startingRow+1 == rows || startingCol+1 == cols )
-                break;
-
-            int prev = mat.get(startingRow+1).get(startingCol);
-                
-    
+            int elementToReplace = mat.get(startingRow + 1).get(startingCol);
+            
+            
+            //Setting 1st Row in O/P
             for(int idx = startingCol; idx < cols; idx++){
-               count++;
-                curr = mat.get(startingRow).get(idx);
-                mat.get(startingRow).set(idx,prev);
-                prev = curr;
+                count++;
+                currElement = mat.get(startingRow).get(idx);
+                mat.get(startingRow).set(idx,elementToReplace);
+                elementToReplace = currElement;
             }
             startingRow++;
-          
-
-            for(int idx = startingRow; idx < rows; idx++){
-               count++;
-                curr = mat.get(idx).get(cols-1);
-                mat.get(idx).set(cols-1,prev);
-                prev = curr;
-            }
             
+            //Setting Last Column in O/P
+            for(int idx = startingRow; idx < rows; idx++){
+                count++;
+                currElement = mat.get(idx).get(cols - 1);
+                mat.get(idx).set(cols - 1,elementToReplace);
+                elementToReplace = currElement;
+            }
             cols--;
             
-
-            if(startingRow <rows ){
-            for(int idx = cols-1; idx >= startingCol; idx--){
-                count++;
-                 curr = mat.get(rows-1).get(idx);
-                mat.get(rows-1).set(idx,prev);
-                prev = curr;
+            //Setting Last row in O/P
+            if(startingRow < rows){
+                for(int idx = cols - 1; idx >= startingCol; idx--){
+                    count++;
+                    currElement = mat.get(rows - 1).get(idx);
+                    mat.get(rows - 1).set(idx,elementToReplace);
+                    elementToReplace = currElement;
+                }
             }
-            }
-            rows--;
-           
             
-            if(startingCol < cols ){
-            for(int idx = rows-1; idx >= startingRow; idx--){
-                count++;
-                 curr = mat.get(idx).get(startingCol);
-                mat.get(idx).set(startingCol,prev);
-                prev = curr;
+            rows--;
+            
+            //Setting 1st column in O/P
+            if(startingCol < cols){
+                for(int idx = rows - 1; idx >= startingRow; idx--){
+                    count++;
+                    currElement = mat.get(idx).get(startingCol);
+                    mat.get(idx).set(startingCol,elementToReplace);
+                    elementToReplace = currElement;
+                }
             }
-            }
-        startingCol++;
+            startingCol++;
+        }
     }
-}
 }
